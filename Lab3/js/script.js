3de5c03db9965b94f6a767ad8c2ae55d7dd43c3e
Sharon-2309 <sharonkwalipeter@gmail.com>
2026-04-13 11:08:41 +1000

    Remove IS312 Lab 3 files

--- Lab3/js/script.js
+++
@@ -1,6 +0,0 @@
-// jQuery document ready event
-// Ensures the page is fully loaded before executing JavaScript
-
-$(document).ready(function () {
-    console.log("jQuery is successfully running on this page.");
-});
